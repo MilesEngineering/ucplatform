d4f94537f8343dd48be684100be6fe1faaa583bc
Miles Gazic <mgazic@github.com>
2021-01-28 21:02:00 -0700

    Add Segger J-Link RTT support.

--- debug_printf.cpp
+++ debug_printf.cpp
@@ -10,6 +10,9 @@
 // uncomment to output strings inside messages, instead of format string ID and arg list
 //#define OUTPUT_DEBUG_STRINGS
 
+// uncomment to output strings to Segger J-Link RTT
+#define USE_SEGGER_RTT
+
 #ifdef OUTPUT_DEBUG_STRINGS
 #define PRIORITY_TYPE PrintfMessage::Priorities
 #else
@@ -17,9 +20,28 @@
 #define PRIORITY_TYPE PrintfIDMessage::Priorities
 #endif
 
+#ifdef USE_SEGGER_RTT
+#include "rtt/RTT/SEGGER_RTT.h"
+#define SEGGER_RTT_BUFFER_INDEX_FOR_PRINT 0
+#endif
+
 void vLogStatement(PRIORITY_TYPE priority, int format_id, const char *fmt, int argc, va_list argp)
 {
     int stream_id = MessageClient::CurrentClientID();
+#ifdef USE_SEGGER_RTT
+    {
+        //# Don't do string formatting here.  Instead call SEGGER_RTT_Write()
+        //# or SEGGER_RTT_WriteNoLock/SEGGER_RTT_WriteSkipNoLock/
+        //# SEGGER_RTT_ASM_WriteSkipNoLock/SEGGER_RTT_WriteWithOverwriteNoLock
+        //# to write binary data, and then update MsgDebug on PC to read from
+        //# RTT (either directly via Segger's API, or by opening telnet on port
+        //# 19021 to a Segger RTT server.
+        va_list copy;
+        va_copy(copy, argp);
+        SEGGER_RTT_vprintf(SEGGER_RTT_BUFFER_INDEX_FOR_PRINT, fmt, &copy);
+        va_end(copy);
+    }
+#endif
 #ifdef OUTPUT_DEBUG_STRINGS
     UNUSED(format_id);
     MessageBuffer* buf = DebugServer::GetBuffer(PrintfIDMessage::MSG_SIZE);
@@ -65,22 +87,22 @@ void vLogStatement(PRIORITY_TYPE priority, int format_id, const char *fmt, int a
 
 void _debug_printf(int count, int format_id, const char *fmt, ...)
 {
-	va_list argp;
-	va_start(argp, fmt);
-	vLogStatement(PRIORITY_TYPE::Info, format_id, fmt, count, argp);
-	va_end(argp);
+    va_list argp;
+    va_start(argp, fmt);
+    vLogStatement(PRIORITY_TYPE::Info, format_id, fmt, count, argp);
+    va_end(argp);
 }
 void _debug_warn(int count, int format_id, const char *fmt, ...)
 {
-	va_list argp;
-	va_start(argp, fmt);
-	vLogStatement(PRIORITY_TYPE::Warning, format_id, fmt, count, argp);
-	va_end(argp);
+    va_list argp;
+    va_start(argp, fmt);
+    vLogStatement(PRIORITY_TYPE::Warning, format_id, fmt, count, argp);
+    va_end(argp);
 }
 void _debug_error(int count, int format_id, const char *fmt, ...)
 {
-	va_list argp;
-	va_start(argp, fmt);
-	vLogStatement(PRIORITY_TYPE::Error, format_id, fmt, count, argp);
-	va_end(argp);
+    va_list argp;
+    va_start(argp, fmt);
+    vLogStatement(PRIORITY_TYPE::Error, format_id, fmt, count, argp);
+    va_end(argp);
 }
